344a4c8da868ccfcc1f9324bc6eb3e64ef7338cb
Jmingpt <88297237+Jmingpt@users.noreply.github.com>
2022-07-29 15:45:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if 'fb' not in st.session_state:
 def run():
     st.set_page_config(page_title="PT MMM Model", 
                        page_icon="📈", 
-                       layout="centered", # centered, wide
+                       layout="wide", # centered, wide
                        initial_sidebar_state="auto")
     
     title_col = st.columns((2,2))
